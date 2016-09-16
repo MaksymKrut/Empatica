c1ccb5310dd59f2f5afd0604c7c4018f7b3b5965
Maksym Krutskykh <maximkrut@yahoo.com>
2016-09-16 17:55:32 +0200

    WebDriver setup

--- steps/home.py
+++ steps/home.py
@@ -4,3 +4,24 @@
 @github 'https://gist.github.com/MaksymKrut'
 '''
 
+
+from behave import *
+import time
+from selenium import webdriver
+
+
+@given("I am on the home page")
+def step_impl(context):
+    context.browser.get("https://www.empatica.com/")
+    context.browser.find_element_by_id("storeDropdown")
+    context.browser.find_element_by_id("storeDropdown")
+
+
+def step_impl(context):
+
+
+
+def step_impl(context):
+
+
+
